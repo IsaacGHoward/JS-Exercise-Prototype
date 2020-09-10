a27cf30071ecaaaf461b19c4337b273446983ace
IsaacGHoward <howardisaac100@gmail.com>
2020-09-09 17:15:12 -0700

    Task 1, and 2 + stretch

--- index.js
+++ index.js
@@ -39,8 +39,20 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  this.stomach = [];
+}
+Person.prototype.eat = function(someFood){
+  if(this.stomach.length >= 10) return;
+  this.stomach.push(someFood);
+}
+Person.prototype.poop = function(){
+  this.stomach = [];
+}
+Person.prototype.toString = function(){
+  return(this.name + ", " + this.age)
 }
 
 /*
@@ -57,10 +69,28 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
+function Car(model, milesPerGallon) {
+  this.model = model;
+  this.milesPerGallon = milesPerGallon;
+  this.tank = 0;
+  this.odometer = 0;
+}
+Car.prototype.fill = function(gallons){
+  this.tank += gallons;
+}
+Car.prototype.drive = function(distance){
+  let gallonsToLose = distance / this.milesPerGallon;
+  for(let i=0;i<gallonsToLose*2;i++){
+    if(this.tank - .5 >= 0){
+      this.tank-=.5;
+      this.odometer += this.milesPerGallon * .5;
+    }
+    else{
+      return("I ran out of fuel at " + this.odometer + " miles!");
+    }
+  }
 
 }
-
 /*
   TASK 3
     - Write a Baby constructor subclassing Person.
